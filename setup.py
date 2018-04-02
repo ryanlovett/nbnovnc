540aa39ba7569d8fbf23ac67e9df8a2764de3b4d
ryanlovett <rylo@berkeley.edu>
2018-04-02 10:45:06 -0700

    Add author_email.

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setuptools.setup(
     version='0.0.2',
     url="https://github.com/ryanlovett/nbnovnc",
     author="Ryan Lovett",
+    author_email="rylo@berkeley.edu",
     description="Jupyter extension to proxy a VNC session via noVNC",
     packages=setuptools.find_packages(),
 	keywords=['Jupyter'],

fb370da6a7d47ba03d58c6157c721c605003ae16
ryanlovett <rylo@berkeley.edu>
2018-04-17 10:46:15 -0700

    Require nbserverproxy 0.7.1. Bump to 0.0.3.

--- nbnovnc/circus.py
+++
@@ -1,5 +0,0 @@
-import time
-
-def sleep(*args, **kw):
-    time.sleep(5)
-    return True
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="nbnovnc",
-    version='0.0.2',
+    version='0.0.3',
     url="https://github.com/ryanlovett/nbnovnc",
     author="Ryan Lovett",
     author_email="rylo@berkeley.edu",
@@ -12,7 +12,7 @@ setuptools.setup(
 	classifiers=['Framework :: Jupyter'],
     install_requires=[
         'notebook',
-        'nbserverproxy >= 0.7'
+        'nbserverproxy >= 0.7.1'
     ],
     package_data={'nbnovnc': ['static/*']},
 )
